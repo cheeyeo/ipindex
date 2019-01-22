4ce4127e4673013aeb0928f34996c31609853706
Chee Yeo <ckyeo.1@gmail.com>
2019-01-22 19:10:35 +0000

    Revert changes

--- ipindex-concurrent.py
+++ ipindex-concurrent.py
@@ -2,10 +2,12 @@ import os
 import gzip
 import multiprocessing as mp
 import re
+import shutil
 import uuid
 from netaddr import iprange_to_cidrs
 import pymongo
 from pymongo import MongoClient
+from pymongo import InsertOne
 from pymongo.write_concern import WriteConcern
 
 
@@ -18,34 +20,34 @@ FILE_LIST = {
       "ripe.db.inetnum.gz",
       "ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.inetnum.gz",
     ],
-    # "ripe-ipv6": [
-    #   "ripe.db.inet6num.gz",
-    #   "ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.inet6num.gz",
-    # ],
-    # "arin": [
-    #   "arin.db",
-    #   "ftp://ftp.arin.net/pub/rr/arin.db"
-    # ],
-    # "afrinic": [
-    #   "afrinic.db.gz",
-    #   "ftp://ftp.afrinic.net/pub/dbase/afrinic.db.gz",
-    # ],
-    # "apnic": [
-    #   "apnic.db.inetnum.gz",
-    #   "ftp://ftp.apnic.net/pub/apnic/whois/apnic.db.inetnum.gz",
-    # ],
-    # "apnic-ipv6": [
-    #   "apnic.db.inet6num.gz",
-    #   "ftp://ftp.apnic.net/pub/apnic/whois/apnic.db.inet6num.gz",
-    # ],
+    "ripe-ipv6": [
+      "ripe.db.inet6num.gz",
+      "ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.inet6num.gz",
+    ],
+    "arin": [
+      "arin.db",
+      "ftp://ftp.arin.net/pub/rr/arin.db"
+    ],
+    "afrinic": [
+      "afrinic.db.gz",
+      "ftp://ftp.afrinic.net/pub/dbase/afrinic.db.gz",
+    ],
+    "apnic": [
+      "apnic.db.inetnum.gz",
+      "ftp://ftp.apnic.net/pub/apnic/whois/apnic.db.inetnum.gz",
+    ],
+    "apnic-ipv6": [
+      "apnic.db.inet6num.gz",
+      "ftp://ftp.apnic.net/pub/apnic/whois/apnic.db.inet6num.gz",
+    ],
 }
 
 def parse_property(block, name):
-  match = re.findall(u"^{0:s}:\s*(.*)$".format(name), block, re.MULTILINE)
-  if match:
-    return " ".join(match).strip()
-  else:
-    return None
+    match = re.findall(u"{0:s}:(.+?)(?=\|.+\:)".format(name), block)
+    if match:
+        return " ".join(match).strip()
+    else:
+        return None
 
 def parse_property_inetnum(block):
     # IPv4
@@ -75,6 +77,63 @@ def parse_property_inetnum(block):
         if match:
             return match[0]
 
+def chunkify(filename, size=1024*1024):
+  fileEnd = os.path.getsize(filename)
+  print("ORIG FILE SIZE:", fileEnd)
+
+  with open(filename, "rb") as f:
+    chunkEnd = f.tell()
+    while True:
+      chunkStart = chunkEnd
+      f.seek(size, 1)
+
+      # incomplete line
+      l = f.readline()
+      l = f.readline()
+      if not l:
+        return
+
+      while l and (b"% RIPE-USER-RESOURCE" in l):
+        p = f.tell()
+        l = f.readline()
+        f.seek(p) # revert one line
+
+      chunkEnd = f.tell()
+      yield chunkStart, chunkEnd - chunkStart
+      if chunkEnd > fileEnd:
+        break
+
+def process_wrapper(chunkStart, chunkSize, filename, source):
+  print("Process id:", os.getpid())
+
+  single_block = ""
+  blocks = []
+
+  with open(filename, mode="rt", encoding="ISO-8859-1") as f:
+    f.seek(chunkStart)
+    lines = f.read(chunkSize).splitlines()
+
+    for line in lines:
+      if (
+      line.startswith("%")
+      or line.startswith("#")
+      or line.startswith("remarks:")
+      or line.startswith(" ")):
+        continue
+
+      if line.strip() == "":
+        if single_block.startswith("inetnum:") or single_block.startswith("inet6num:"):
+          blocks.append(single_block)
+          single_block = ""
+        else:
+          single_block = ""
+      else:
+        single_block += line + '|'
+
+  parsed_blocks = parse_blocks(blocks, source)
+  print("Parsed {:d} records".format(len(parsed_blocks)))
+  return parsed_blocks
+
 def parse_blocks(blocks, source):
   res = list()
   for block in blocks:
@@ -92,66 +151,6 @@ def parse_blocks(blocks, source):
     res.append(data)
 
   return res
-
-def file_block(fp, number_of_blocks, block):
-  '''
-  A generator that splits a file into blocks and iterates
-  over the lines of one of the blocks.
-
-  '''
-  assert 0 <= block and block < number_of_blocks
-  assert 0 < number_of_blocks
-
-  fp.seek(0,2)
-  file_size = fp.tell()
-
-  ini = file_size * block / number_of_blocks
-  end = file_size * (1 + block) / number_of_blocks
-
-  if ini <= 0:
-    fp.seek(0)
-  else:
-    fp.seek(ini-1)
-    fp.readline()
-
-  while fp.tell() < end:
-    yield fp.readline()
-
-
-def process_wrapper(filename, num_workers, chunk_number, source):
-  print("Process id: {} Filename: {} Num workers: {} Chunk: {}".format(os.getpid(), filename, num_workers, chunk_number))
-
-  if filename.endswith(".gz"):
-    fp = gzip.open(filename, mode="rt", encoding="ISO-8859-1")
-  else:
-    fp = open(filename, mode="rt", encoding="ISO-8859-1")
-
-
-  single_block = ""
-  blocks = []
-
-  for line in file_block(fp, num_workers, chunk_number):
-    if (
-      line.startswith("%")
-      or line.startswith("#")
-      or line.startswith("remarks:")
-      or line.startswith(" ")):
-        continue
-
-    if line.strip() == "":
-      if single_block.startswith("inetnum:") or single_block.startswith("inet6num:"):
-        blocks.append(single_block)
-        single_block = ""
-      else:
-        single_block = ""
-    else:
-      single_block += line
-
-  fp.close()
-  parsed_blocks = parse_blocks(blocks, source)
-  print("Parsed {:d} records".format(len(parsed_blocks)))
-  return parsed_blocks
-
 
 def connect_mongodb():
   """ mongodb connection """
@@ -197,15 +196,21 @@ def rebuild_indexes(client, collection):
 def main():
   client = connect_mongodb()
 
-  num_workers = 4
-  pool = mp.Pool(num_workers)
+  pool = mp.Pool(4)
   jobs = []
 
   for key, value in FILE_LIST.items():
-    filename = "data/" + value[0]
+    archive = "data/" + value[0]
+    filename = "data/" + value[0].replace(".gz", "")
+    print("[INFO] parsing", filename)
+    # extract gzip file
+    with gzip.open(archive, mode="rb") as f_in:
+      with open(filename, "wb") as f_out:
+        shutil.copyfileobj(f_in, f_out)
 
-    for chunk_number in range(num_workers):
-      jobs.append(pool.apply_async(process_wrapper,(filename, num_workers, chunk_number, key.upper())))
+    for chunkStart, chunkSize in chunkify(filename):
+      print("chunkStart: {}, chunkSize: {}".format(chunkStart, chunkSize))
+      jobs.append(pool.apply_async(process_wrapper,(chunkStart,chunkSize, filename, key.upper())))
 
     count = 0
     for job in jobs:
@@ -216,6 +221,9 @@ def main():
       print("Mongodb Update: ", res)
 
     print("Total records imported: {:d}".format(count))
+
+    # delete extracted file
+    os.remove(filename)
 
   pool.close()
   exit(0)
